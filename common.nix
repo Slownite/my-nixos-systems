91d2289150dbae5dbbced17ca4ecde37f3e41321
Slownite <snfdiop@outlook.com>
2024-04-07 01:17:47 +0200

    add desktop.nix homelab.nix common.nix and the setup.py script to install everything in /etc/nixos/

---
+++ common.nix
@@ -0,0 +1,79 @@
+{ config, pkgs, ... }:
+
+{
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-fbdacde1-76e0-4d9c-89cc-e0cab5e7d2d4".device = "/dev/disk/by-uuid/fbdacde1-76e0-4d9c-89cc-e0cab5e7d2d4";
+  networking.hostName = "nixos"; # Define your hostname.
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    bat
+    wget
+    curl
+    git
+    zsh
+    fzf
+    tmux
+    #build-essentials
+    bison
+    flex
+    fontforge
+    makeWrapper
+    pkg-config
+    gnumake
+    gcc
+    libiconv
+    autoconf
+    automake
+    libtool
+    cmake
+    ###
+    docker
+    docker-compose
+    poetry
+    python3
+  ];
+  #programs configuration
+  virtualisation.docker.enable = true;
+  programs.zsh = {
+    enable = true;
+  };
+  programs.git = {
+    enable = true;
+  };
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
--- desktop.nix
+++ desktop.nix
@@ -5,43 +5,17 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    /etc/nixos/hardware-configuration.nix
+    ./common.nix
+  ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.initrd.luks.devices."luks-fbdacde1-76e0-4d9c-89cc-e0cab5e7d2d4".device = "/dev/disk/by-uuid/fbdacde1-76e0-4d9c-89cc-e0cab5e7d2d4";
-  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fr_FR.UTF-8";
-    LC_IDENTIFICATION = "fr_FR.UTF-8";
-    LC_MEASUREMENT = "fr_FR.UTF-8";
-    LC_MONETARY = "fr_FR.UTF-8";
-    LC_NAME = "fr_FR.UTF-8";
-    LC_NUMERIC = "fr_FR.UTF-8";
-    LC_PAPER = "fr_FR.UTF-8";
-    LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_TIME = "fr_FR.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -87,69 +61,39 @@
     isNormalUser = true;
     description = "Sam";
     shell = pkgs.zsh;
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      flatpak
+    extraGroups = [
+      "networkmanager"
+      "wheel"
     ];
+    packages = with pkgs; [ flatpak ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
   services.flatpak.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  bat
-  wget
-  git
-  zsh
-  stow
-  emacs
-  fzf
-  atuin
-  lazygit
-  tmux
-  nodejs_20
-  html-tidy
-  powerline-fonts
-  shellcheck
-  shfmt
-  cmake
-  fd
-  pandoc
-  #build-essentials
-  bison
-  flex
-  fontforge
-  makeWrapper
-  pkg-config
-  gnumake
-  gcc
-  libiconv
-  autoconf
-  automake
-  libtool
-  ###
-  ripgrep
-  zoxide
-  docker
-  docker-compose
-  poetry
-  python3
-  nerdfonts
-  zsh-autosuggestions
-  pipx
-  syncthing
+    stow
+    emacs
+    atuin
+    lazygit
+    nodejs_20
+    html-tidy
+    powerline-fonts
+    shellcheck
+    shfmt
+    cmake
+    fd
+    pandoc
+    ripgrep
+    zoxide
+    nerdfonts
+    zsh-autosuggestions
+    pipx
+    syncthing
   ];
   #programs configuration
-  programs.zsh = {
-    enable = true;
-  };
-  programs.git = {
-    enable = true;
-  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -168,13 +112,4 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-
 }
