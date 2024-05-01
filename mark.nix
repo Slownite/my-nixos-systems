5f87a97fc2b9d627cebdeeb6a31eb83bbe461d32
Slownite <snfdiop@outlook.com>
2024-05-01 22:59:55 +0200

    split server config

---
+++ mark.nix
@@ -0,0 +1,48 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    /etc/nixos/hardware-configuration.nix
+    ./common.nix
+    ./homelab/common-homelab.nix
+  ];
+
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hostName = "mark";
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "fr";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  users.motd = ''
+       /\     /\
+    {  `---'  }
+    {  O   O  }
+    ~>  V  <~~
+     \  \|/  /
+      `-----'____
+      /     \    \_
+     {       }\  )_\_   _
+     |  \_/  |/ /  \_\_/ )
+      \__/  /(_/     \__/
+        (__/
+
+  '';
+}
