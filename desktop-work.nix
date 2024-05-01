6bd152ec2aca4a18f1125bda5acf41d951a36902
Slownite <snfdiop@outlook.com>
2024-05-01 22:58:49 +0200

    split config

--- desktop-work.nix
+++ desktop-work.nix
@@ -9,125 +9,14 @@
     # Include the results of the hardware scan.
     /etc/nixos/hardware-configuration.nix
     ./common.nix
+    ./work/common-work.nix
   ];
 
   boot.initrd.luks.devices."luks-fbdacde1-76e0-4d9c-89cc-e0cab5e7d2d4".device = "/dev/disk/by-uuid/fbdacde1-76e0-4d9c-89cc-e0cab5e7d2d4";
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-  plasma-browser-integration
-  oxygen
-];
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "fr";
-    xkbVariant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "fr";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.sam = {
-    isNormalUser = true;
-    description = "Sam";
-    shell = pkgs.zsh;
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    packages = with pkgs; [ 
-    flatpak
-    black
-    pipenv
-    isort
-    python311Packages.nose3
-    python311Packages.pytest
-    python311Packages.setuptools
-    python311Packages.pip
-    nixfmt
-    zig
-
-    ];
-  };
-
-  services.flatpak.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    stow
-    emacs
-    atuin
-    lazygit
-    nodejs_20
-    html-tidy
-    powerline-fonts
-    shellcheck
-    shfmt
-    cmake
-    fd
-    pandoc
-    ripgrep
-    zoxide
-    nerdfonts
-    zsh-autosuggestions
-    pipx
-    syncthing
-    obs-studio
-  ];
-  #programs configuration
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.hostName = "work"; # Define your hostname.
+  networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
 }
