3f9f7b692c1ca7b265bbb534c554b501026fa5bc
Slownite <snfdiop@outlook.com>
2024-05-04 21:57:34 +0200

    delete nfs port

--- mark.nix
+++ mark.nix
@@ -70,11 +70,9 @@
     9443 # portainer
     8096 # jellyfin
     53 # pi-hole
-    2049 # nfs
   ];
   networking.firewall.allowedUDPPorts = [
     53 # pi-hole
     67 # Only required if you are using Pi-hole as your DHCP server
-    2049 # nfs
   ];
 }
