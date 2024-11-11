7cd1b9fd3dd3aa0b9a4944d65e2469fa642aa22c
Samuel Diop <snfdiop@outlook.com>
2024-11-11 20:54:56 +0100

    hello docker

--- hosts/homelab/hermes/configuration.nix
+++ hosts/homelab/hermes/configuration.nix
@@ -16,7 +16,7 @@
     enable = true;
     name = "Hermes";
   };
-  vaultwarden.enable = true;
+  vaultwarden.enable = false;
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
