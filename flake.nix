4b7a283e1ce2aaf6fc75a8f5785e07888ad64d04
Samuel Diop <36275278+Slownite@users.noreply.github.com>
2025-09-14 21:36:35 +0200

    Update flake.nix
    move to unstable branch

--- flake.nix
+++ flake.nix
@@ -1,29 +1,25 @@
 {
-  description = "Reproductible build of my different nix os hosts";
+  description = "Reproducible build of my different NixOS hosts";
 
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs?ref=nixos-24.05"; };
-    nixpkgs-unstable = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    nixpkgs = { url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
     stylix = {
-      url = "github:danth/stylix/release-24.05";
+      url = "github:danth/stylix/release-unstable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-unstable";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.stylix.follows = "stylix";
     };
   };
 
-  outputs = { self, nixpkgs, stylix, home-manager, nixpkgs-unstable, ... }:
+  outputs = { self, nixpkgs, stylix, home-manager, ... }:
     let
       system = "x86_64-linux";
-
       pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      pkgs-unstable = import nixpkgs-unstable {
         inherit system;
         config.allowUnfree = true;
       };
@@ -31,34 +27,23 @@
       nixosConfigurations = {
         homeStation = nixpkgs.lib.nixosSystem {
           specialArgs = {
-            inherit system;
-            inherit pkgs-unstable;
-            inherit stylix;
+            inherit system stylix;
+            # Add anything else you want available to modules here
           };
           modules = [
             home-manager.nixosModules.home-manager
             ./hosts/home_station/configuration.nix
           ];
         };
-        homeLaptop = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit system;
-            inherit pkgs-unstable;
-            inherit stylix;
-          };
-          modules = [ 
-          home-manager.nixosModules.home-manager
-          ./hosts/mac2020/configuration.nix 
-          ];
-        };
       };
+
       homeConfigurations = {
         sam = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          extraSpecialArgs = { inherit pkgs-unstable; };
-          modules =
-            [ stylix.homeManagerModules.stylix ./hosts/home_station/home.nix 
-            ];
+          modules = [
+            stylix.homeManagerModules.stylix
+            ./hosts/home_station/home.nix
+          ];
         };
       };
     };
