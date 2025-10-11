55c5e438b1a752edb746f9954861727d0b093e7c
Samuel Diop <snfdiop@outlook.com>
2025-10-11 23:38:46 +0200

    add zoxide to shell

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -9,16 +9,21 @@
     ./fastfetch.nix
     ./claude_code.nix
   ];
-  home.packages = with pkgs; [ bat wget curl fzf gh
-
+  home.packages = with pkgs; [
+    bat
+    wget
+    curl
+    fzf
+    gh
     htop
+    zoxide
     nvtopPackages.full
     nix-index
     zip
     unzip
     fabric-ai
     dvc
-   ffmpeg
+    ffmpeg
     nixos-generators
     claude-code
     
