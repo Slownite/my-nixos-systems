ab09cc33753e52b95df6e78428a20f069c391758
Samuel Diop <snfdiop@outlook.com>
2025-12-29 00:46:34 +0100

    fix(nixos): change nvtop from user space to systems space.

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -1,19 +1,23 @@
 { pkgs, ... }:
 {
-	      nixpkgs.config.allowUnfree = true;
-            # Use the proprietary NVIDIA drivers
-            services.xserver.videoDrivers = [ "nvidia" ];
-	    hardware.nvidia = {
-	  open = false;
-	  modesetting.enable = true;
-	  nvidiaSettings = true;
-	};
-            # Ensure necessary firmware is included
-            hardware.firmware = [
-              pkgs.linux-firmware
-            ];
+  nixpkgs.config.allowUnfree = true;
+  # Use the proprietary NVIDIA drivers
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    open = false;
+    modesetting.enable = true;
+    nvidiaSettings = true;
+  };
+  # Ensure necessary firmware is included
+  hardware.firmware = [
+    pkgs.linux-firmware
+  ];
 
-            boot.extraModulePackages = [
-              pkgs.linuxPackages.nvidia_x11
-            ];
+  boot.extraModulePackages = [
+    pkgs.linuxPackages.nvidia_x11
+  ];
+
+  environment.systemPackages = with pkgs; [
+    nvtopPackages.full
+  ];
 }
